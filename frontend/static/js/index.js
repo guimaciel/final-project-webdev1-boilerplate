68c422e738552a6ff339d9c43152500e529a4be6
Guilherme Maciel de Almeida <guilherme@gmaciel.com>
2022-05-18 10:48:03 -0700

    Add search countries

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -3,3 +3,49 @@ import {getCountries} from './countries.js'
 const countries = getCountries();
 
 console.log(countries);
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    const formSearch = document.getElementById("formSearch");
+    formSearch.addEventListener('submit',function(event) {
+        event.preventDefault();
+        console.log("submit");
+    });
+    const searchCountry = document.getElementById("searchCountry");
+    searchCountry.addEventListener('keyup',function(event){
+        // if (event.keyCode === 13) {
+        //     console.log("d");
+        // }
+        // console.log(event.keyCode);
+        // console.log(countries[0].capital);
+        
+        const countriesName = this.parentElement.parentElement.parentElement.children[1];
+        for (let i = 0 ; i < countriesName.childElementCount ; i++) {
+            const countryName = countriesName.children[i].children[0].children[1].children[0].innerText;
+            if (countryName.toUpperCase().includes(this.value.toUpperCase())) {
+                countriesName.children[i].children[0].style.display = "block";
+            } else {
+                countriesName.children[i].children[0].style.display = "none";
+            }
+            
+        }
+    
+    });
+
+    const regionSelect = document.getElementById("regionSelect");
+    regionSelect.addEventListener('change',function(event){
+        
+        const countriesRegion = this.parentElement.parentElement.parentElement.children[0].children[1];
+        console.log(this.value);
+        for (let i = 0 ; i < countriesRegion.childElementCount ; i++) {
+            const countryRegion = countriesRegion.children[i].children[0].children[1].children[2].innerText;
+            if (countryRegion == this.value || this.value === "" ) {
+                countriesRegion.children[i].children[0].style.display = "block";
+            } else {
+                countriesRegion.children[i].children[0].style.display = "none";
+            }
+            
+        }
+    });
+});
--- frontend/static/js/views/Countries.js
+++ frontend/static/js/views/Countries.js
@@ -8,22 +8,50 @@ export default class extends AbstractView{
     }
 
     async getHTML() {
-        // return "<h1>Countries</h1>";
-        return this.printPage();
+        const containerCards = document.createElement("div");
+        const countries = getCountries();
+
+        for (let country of countries) {
+          const card = document.createElement("div");
+          card.innerHTML = `
+                <div class="country-cards">
+                    <div class="country-img">
+                        <img src="${country.flags.png}" />
+                    </div>
+                    <div class="country-information">
+                        <h2 id="countryName">${country.name.common}</h2>
+                        <p>Population: ${country.population}</p>
+                        <p id="region">Region: ${country.region}</p>
+                        <p>Capital: ${country.capital}</p>
+                    </div>
+                    </div>
+                </div>
+            `;
+            containerCards.appendChild(card);
+        }
+
+ 
+        return `
+            <section class="countries-section">
+                <div class="filter-container">
+                    <form id="formSearch">
+                        <input type="text" name="searchCountry" id="searchCountry" placeholder="Search for a country..." class="searching-part-inpt">
+                    </form>
+                    <select class="regionSelect" id="regionSelect">
+                        <option value="">Filter by Region</option>
+                        <option value="Region: Africa">Africa</option>
+                        <option value="Region: Americas">Americas</option>
+                        <option value="Region: Asia">Asia</option>
+                        <option value="Region: Europe">Europe</option>
+                        <option value="Region: Oceania">Oceania</option>
+                    </select>
+                </div>
+                <div class="container">
+                    ${containerCards.innerHTML}
+                </div>
+            </section>
+        `;
+      
     }
 
-    printPage() {
-        let html = "<h1>Countries</h1>";
-        const countries = getCountries();
-        let i = 0;
-        for (let country of countries) {
-            i++;
-            html += "<p>" + country.name.common + "</p>";
-            html += "<p>" + country.population + "</p>";
-            html += "<p>" + country.region + "</p>";
-            html += "<p>" + country.capital + "</p>";
-            html += "<img src='" + country.flags.png + "'/></p>";
-        }
-        return html;
-    }
 }
